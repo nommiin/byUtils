397ae272c9cf7d5471b8a2e0b37553ef043b972b
Torin Freimiller <torinfrei@gmail.com>
2018-11-07 11:37:29 -0600

    Initial commit of byUtils

---
+++ Chunks/STRG.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace byUtils {
+    public class Stringy {
+        public UInt32 Offset;
+        public string Value;
+        public long Base;
+        public Stringy(BinaryReader brMain) {
+            this.Offset = brMain.ReadUInt32();
+            this.Base = brMain.BaseStream.Position;
+            brMain.BaseStream.Seek(this.Offset, SeekOrigin.Begin);
+            this.Value = Encoding.ASCII.GetString(brMain.ReadBytes((int)brMain.ReadUInt32()));
+            brMain.BaseStream.Seek(this.Base, SeekOrigin.Begin);
+        }
+    }
+
+    public static class STRG {
+        public static void Read(BinaryReader brMain) {
+            List<Stringy> brStrings = new List<Stringy>();
+            for (int i = 0, _i = (int)brMain.ReadUInt32(); i < _i; i++) {
+                brStrings.Add(new Stringy(brMain));
+                Console.WriteLine(brStrings[brStrings.Count - 1]);
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace byUtils {
+    class Chunk {
+        public string Name;
+        public UInt32 Length;
+        public long Base;
+        public Chunk(BinaryReader brMain) {
+            this.Name = Encoding.ASCII.GetString(brMain.ReadBytes(4));
+            this.Length = brMain.ReadUInt32();
+            this.Base = brMain.BaseStream.Position;
+        }
+
+        public override string ToString() {
+            return "Name: " + this.Name + ", Length: " + this.Length.ToString();
+        }
+    }
+
+    class Program {
+        public static string byFile = @"/Users/torinfreimiller/Library/Application Support/com.yoyogames.macyoyorunner/game/assets/game.ios";
+        static void Main(string[] args) {
+
+            if (File.Exists(byFile) == true) {
+                // Read the file 
+                using (BinaryReader brMain = new BinaryReader(File.OpenRead(byFile))) {
+                    Chunk byForm = new Chunk(brMain);
+                    if (byForm.Name == "FORM") {
+                        while (brMain.BaseStream.Position < byForm.Length) {
+                            Chunk byChunk = new Chunk(brMain);
+                            switch (byChunk.Name) {
+                            case "STRG": {
+                                    STRG.Read(brMain);
+                                    break;
+                                }
+
+                            default: {
+                                    Console.WriteLine("Chunk '" + byChunk.Name + "' is not supported.");
+                                    break;
+                                }
+                            }
+                            brMain.BaseStream.Seek(byChunk.Base + byChunk.Length, SeekOrigin.Begin);
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
